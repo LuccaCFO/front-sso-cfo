67cbe3b397d234579f9361c5d41497f7729f5693
Lucca Serejo <lucca.serejo@cfo.org.br>
2026-04-17 11:57:03 -0300

    fix: changed the header padding for mobile

--- src/features/auth/components/Header.tsx
+++ src/features/auth/components/Header.tsx
@@ -9,19 +9,19 @@ export function Header() {
   return (
     <header className="flex flex-col items-center gap-8">
       <div className="min-w-full flex items-center justify-center flex-wrap bg-cfo-primary text-white">
-        <div className="flex items-center gap-2 px-6 py-3">
+        <div className="flex items-center gap-2 px-6 py-0 mc:py-3">
           <img src={locationSVG} alt="Ícone de seta, indicando localização"/>
           <span>Brasília - DF</span>
         </div>
-        <div className="flex items-center gap-2 px-6 py-3">
+        <div className="flex items-center gap-2 px-6 py-0 mc:py-3">
           <img src={phoneSVG} alt="Ícone de telefone"/>
           <span>(61) 3033-4499 / 3033-4469</span>
         </div>
-        <div className="flex items-center gap-2 px-6 py-3">
+        <div className="flex items-center gap-2 px-6 py-0 mc:py-3">
           <img src={emailSVG} alt="Ícone de uma carta"/>
           <span>cfo@cfo.org.br</span>
         </div>
-        <div className="flex items-center gap-2 px-6 py-3">
+        <div className="flex items-center gap-2 px-6 py-0 mc:py-3">
           <img src={shieldSVG} alt="Ícone de um escudo"/>
           <span>Acesso restrito a usuário autorizados</span>
         </div>
